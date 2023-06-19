1bdb4a7b4a958c6d47fd4a575d924b34635e57c0
MohammedFathyAbdElHafez <mfathy20220802@gmail.com>
2023-06-20 00:10:18 +0300

    create-new-factory

---
+++ database/factories/ItemFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Item>
+ */
+class ItemFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name(),
+            'price' => fake()->randomFloat(null,0,100.00),
+            'seller' => fake()->company(),
+        ];
+    }
+}
