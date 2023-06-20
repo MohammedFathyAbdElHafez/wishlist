296f0079e420045316ab3aac29c446ec1d0a28b8
MohammedFathyAbdElHafez <mfathy20220802@gmail.com>
2023-06-20 22:47:12 +0300

    add-new-form-requests

---
+++ app/Http/Requests/UpdateItemRequest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateItemRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            "name" => "required",
+            "price" => "required",
+            "seller" => "required",
+        ];
+    }
+
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'Field name is required',
+            'price.required' => 'Field price is required',
+            'seller.required' => 'Field seller is required',
+        ];
+    }
+}
