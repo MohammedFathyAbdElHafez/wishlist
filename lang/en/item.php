7ad2cee4c020bfc7ec1bd962c5f18181cb372247
MohammedFathyAbdElHafez <mfathy20220802@gmail.com>
2023-06-20 22:46:24 +0300

    add-language-file

---
+++ lang/en/item.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Item Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the trnslation for the item model.
+    |
+    */
+
+    'index' => 'Items',
+    'show' => 'Item',
+    'created' => 'Item created successfully.',
+    'updated' => 'Item updated successfully.',
+    'deleted' => 'Item deleted successfully.',
+];
