a603025bda91c658987ed733425adfa0d763fb54
MohammedFathyAbdElHafez <mfathy20220802@gmail.com>
2023-06-20 22:49:46 +0300

    modify-controller

--- app/Http/Controllers/Api/ItemController.php
+++ app/Http/Controllers/Api/ItemController.php
@@ -2,53 +2,73 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\AppBaseController;
+use App\Http\Requests\StoreItemRequest;
+use App\Http\Requests\UpdateItemRequest;
+use App\Http\Resources\ItemResource;
 use App\Models\Item;
+use Exception;
 use Illuminate\Http\Request;
 
-class ItemController extends Controller
+class ItemController extends AppBaseController
 {
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
-        $items = Item::latest()->get();
-        return view('items.index',compact('items'));
+        $item = ItemResource::collection(Item::all());
+        return $this->sendResponse($item, trans('item.index'));
+
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreItemRequest $request)
     {
         //
+        $validatedData = $request->validated();
+
+        $item = Item::create($validatedData);
+        $itemSource =  new ItemResource($item);
+        return $this->sendResponse($itemSource, trans('item.created'));
+
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show($id)
     {
         //
-        $item = Item::find($id);
-        return view('items.show',compact('item'));
+        $item =  new ItemResource(Item::find($id));
+        return $this->sendResponse($item, trans('item.show'));
+
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(UpdateItemRequest $request, string $id)
     {
         //
+        $validatedData = $request->validated();
+
+        $item = Item::find($id);
+        $item->update($validatedData);
+        $itemSource = new ItemResource($item);
+        return $this->sendResponse($itemSource, trans('item.updated'));
+
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy($id)
     {
-        //
+        Item::destroy($id);
+        return $this->sendSuccess(trans('item.deleted'));
+
     }
 }
