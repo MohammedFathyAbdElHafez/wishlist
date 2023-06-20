17833596d14468b2a989b1a849739763c60acd51
MohammedFathyAbdElHafez <mfathy20220802@gmail.com>
2023-06-21 00:48:32 +0300

    modify-index-file

--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -1,22 +1,31 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="page-header">
-                    <h2>All Items</h2>
-                </div>
-                @foreach($items as $item)
-                <div class="card">
-                    <div class="card-header">{{$item->name}}</div>
+@csrf
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12 justify-content-center align-items-center">
 
-                    <div class="card-body">
-                        {{$item->price}}
-                    </div>
-                </div>
-                @endforeach
-            </div>
+            <table class="table table-dark table-striped table-bordered" id="items-table">
+                <thead class="table-dark">
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">{{trans('item.name')}}</th>
+                        <th scope="col">{{trans('item.price')}}</th>
+                        <th scope="col">{{trans('item.seller')}}</th>
+                    </tr>
+                </thead>
+                <tbody>
+                </tbody>
+            </table>
+
+
         </div>
     </div>
+</div>
+
+
 @endsection
+
+<script src="{{ asset('js/items/index.js') }}"></script>
+<script src="{{ asset('js/items/drawItemsTable.js') }}"></script>
